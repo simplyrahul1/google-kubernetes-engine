32af45cf80c0df806bd24ac32c1b435c3c017910
Rahul Udayshankar Sinha <simplyrahul@gmail.com>
2023-10-25 01:24:47 +0800

    kubectl config cluster context info

---
+++ 69-Access-To-Multiple-Clusters/kube_commands.sh
@@ -0,0 +1,7 @@
+  cat ~/.kube/config
+  kubectl config view
+  kubectl config clusters
+  kubectl config get-clusters
+  kubectl config get-contexts
+  kubectl config current-context
+  kubectl cluster-info
